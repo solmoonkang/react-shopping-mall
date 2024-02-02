b3f90a4e3538cea4f252044df9f54b24c06da16d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-02 14:14:41 +0900

    feat: Init firebase and setting

---
+++ .firebase/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKXXdlXhVnqlA8WbhLZw67BAaf_NoFQcA",
+  authDomain: "react-shopping-mall-b9c50.firebaseapp.com",
+  projectId: "react-shopping-mall-b9c50",
+  storageBucket: "react-shopping-mall-b9c50.appspot.com",
+  messagingSenderId: "405832282195",
+  appId: "1:405832282195:web:e45082fe2f849bbd3196a6",
+  measurementId: "G-F3NF6S99ZR"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
