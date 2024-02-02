e7fcca84dbb8f0a4b3cd2091cc7cc36411f682e0
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-02 19:04:55 +0900

    feat: Implement cart item in cart

---
+++ src/components/CartItem.js
@@ -0,0 +1,60 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import styled from "styled-components";
+
+const CartItem = ({ item, addToCart, removeFromCart }) => (
+  <Wrapper>
+    <div>
+      <h3>{item.title}</h3>
+      <div className="information">
+        <p>Price: ${item.price}</p>
+        <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+      </div>
+      <div className="buttons">
+        <Button
+          size="small"
+          disableElevation
+          variant="contained"
+          onClick={() => removeFromCart(item.id)}
+        >
+          -
+        </Button>
+        <p>{item.amount}</p>
+        <Button
+          size="small"
+          disableElevation
+          variant="contained"
+          onClick={() => addToCart(item)}
+        >
+          +
+        </Button>
+      </div>
+    </div>
+    <img src={item.image} alt={item.title} />
+  </Wrapper>
+);
+
+export default CartItem;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-family: Arial, Helvetica, sans-serif;
+  border-bottom: 1px solid lightblue;
+  padding-bottom: 20px;
+
+  div {
+    flex: 1;
+  }
+
+  .information,
+  .buttons {
+    display: flex;
+    justify-content: space-between;
+  }
+  img {
+    max-width: 80px;
+    object-fit: cover;
+    margin-left: 40px;
+  }
+`;
