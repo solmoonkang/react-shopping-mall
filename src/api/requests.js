7f7121db077341558ce3367a29044dbe43406cf8
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-02 14:16:26 +0900

    feat: Implement requests url in requests.js

---
+++ src/api/requests.js
@@ -0,0 +1,11 @@
+const requests = {
+    fetchAllProducts: "/products",
+    fetchElectronics: "/products/category/electronics",
+    fetchJewelery: "/products/category/jewelery",
+    fetchMenClothing: "/products/category/men's clothing",
+    fetchWomenClothing: "/products/category/women's clothing",
+    fetchAllCart: "/carts",
+    fetchUserCart: "/carts/user/2",
+}
+
+export default requests;
