77d8db5718caa5882b9b35711232f1b2d8f7e6db
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-03 17:55:13 +0900

    feat: Implement cart reducer

---
+++ src/reducers/cartReducer.js
@@ -0,0 +1,31 @@
+const cartReducer = (state = [], action) => {
+
+    switch (action.type) {
+        case "ADD_TO_CART": 
+
+            const item = action.payload;
+            const isItemInCart = state.find((i) => i.id === item.id);
+
+            if (isItemInCart) {
+                return state.map((i) => 
+                    i.id === item.id ? { ...i, quantity: item.quantity + 1 } : i);
+            }
+            return [...state, { ...item, quantitiy: 1 }];
+        
+        case "REMOVE_FROM_CART":
+
+            return state.reduce((ack, item) => {
+                if (item.id === action.payload) {
+                    if (item.quantity === 1) return ack;
+                    return [...ack, {...item, quantity: item.quantity - 1 }];
+                } else {
+                    return [...ack, item];
+                }
+            }, []);
+
+        default:
+            return state;
+    }
+};
+
+export default cartReducer;
