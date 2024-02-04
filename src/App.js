0af7122095d86f978a6501a095e9169463a6cb89
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-04 19:05:24 +0900

    feat: Implement App.js

--- src/App.js
+++ src/App.js
@@ -1,90 +1,26 @@
-import React, { useState } from "react";
-import { useQuery } from "react-query";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import Banner from "./components/Banner";
+import Row from "./components/Row";
+import MainPage from "./pages/MainPage";
+
 import "./App.css";
 
-import Item from "./components/Item";
-import Cart from "./components/Cart";
-import Nav from "./components/Nav";
+function App () {
 
-import styled from "styled-components";
-import IconButton from "@material-ui/core/IconButton";
-import { LinearProgress, Drawer, Grid } from "@material-ui/core";
-
-
-const getProducts = async () => {
-  return await (await fetch("https://fakestoreapi.com/products")).json();
-}
-
-const App = () => {
-
-  const [cartOpen, setCartOpen] = useState(false);
-  const [cartItems, setCartItems] = useState([]);
-  const { data, isLoading, error } = useQuery("products", getProducts);
-
-  const getTotalItems = (items) => {
-      return items.reduce((ack, item) => {
-          return ack + item.amount;
-      }, 0);
-  }
-
-  const handleAddToCart = (clickedItem) => {
-      setCartItems((prev) => {
-          const isItemInCart = prev.find((item) => item.id === clickedItem.id);
-
-          if (isItemInCart) {
-              return prev.map((item) => 
-                  item.id === clickedItem.id ? { ...item, amount: item.amount + 1 } : item
-              );
-          }
-          return [...prev, { ...clickedItem, amount: 1 }];
-      });
-  }
-
-  const handleRemoveFromCart = (id) => {
-      setCartItems((prev) => 
-          prev.reduce((ack, item) => {
-              if (item.id === id) {
-                  if (item.amount === 1) return ack;
-                  return [...ack, { ...item, amount: item.amount - 1 }];
-              } else {
-                  return [...ack, item];
-              }
-          }, [])
-      );
-  }
-
-  if (isLoading) return <LinearProgress />;
-
-  if (error) return <div>ERROR!</div>
-
-  return (
-      <Wrapper>
-          <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
-              <Cart cartItems={cartItems} addToCart={handleAddToCart} removeFromCart={handleRemoveFromCart}></Cart>
-          </Drawer>
-          <Nav cartItems={cartItems} setCartOpen={setCartOpen} getTotalItems={getTotalItems} />
-          <div className="grid-container">
-            <Grid container spacing={5}>
-                {data?.map((item) => (
-                    <Grid item key={item.id} xs={3} sm={78}>
-                        <Item item={item} handleAddToCart={handleAddToCart} />
-                    </Grid>
-                ))}
-            </Grid>
-          </div>
-      </Wrapper>
-  );
+    return (
+        <Router>
+            <div>
+                <Nav />
+                <Banner />
+                <Row />
+                <Routes>
+                    <Route path="/" index element={<MainPage />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
-
-const Wrapper = styled.div`
-  margin: 40px;
-`;
-
-const StyledButton = styled(IconButton)`
-  position: fixed;
-  z-index: 100;
-  right: 20px;
-  top: 20px;
-`;
