5de9ad0f19dd991537e88f5ac10fba076e63de23
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-04 19:04:08 +0900

    chore: Clear to read

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -2,6 +2,8 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import styled from "styled-components";
 
+// 여기서는 Cart 컴포넌트에 들어간 각 상품에 대한 정보가 담긴 컴포넌트이다. 여기서 상품명과 상품가격을 출력한다.
+
 const CartItem = ({ item, addToCart, removeFromCart }) => (
   <Wrapper>
     <div>
@@ -11,23 +13,9 @@ const CartItem = ({ item, addToCart, removeFromCart }) => (
         <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
       </div>
       <div className="buttons">
-        <Button
-          size="small"
-          disableElevation
-          variant="contained"
-          onClick={() => removeFromCart(item.id)}
-        >
-          -
-        </Button>
+        <Button size="small" disableElevation variant="contained" onClick={() => removeFromCart(item.id)}>-</Button>
         <p>{item.amount}</p>
-        <Button
-          size="small"
-          disableElevation
-          variant="contained"
-          onClick={() => addToCart(item)}
-        >
-          +
-        </Button>
+        <Button size="small" disableElevation variant="contained" onClick={() => addToCart(item)}>+</Button>
       </div>
     </div>
     <img src={item.image} alt={item.title} />
