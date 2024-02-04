2f7fd2a882585087a5c2a704a790ae45ad20bb4d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-04 19:03:24 +0900

    feat: Implement Nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,28 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import LockOpenIcon from "@material-ui/icons/LockOpen";
-import "./Nav.css";
 import { Badge, IconButton } from "@material-ui/core";
+import { useSelector} from "react-redux";
+import "./Nav.css";
 
-export default function Nav({ cartItems, setCartOpen, getTotalItems }) {
+// NAV 컴포넌트: 로고, 장바구니, 사용자 페이지, 로그인 버튼 등의 네비게이션 요소를 담당한다.
 
+function Nav() {
+
+    const cartItems = useSelector((state) => state.cart);
+    const totalItems = cartItems.reduce((count, item) => count + item.quantity, 0);
+    const navigate = useNavigate();
+    
     return (
         <nav className="nav">
             <div className="nav__logo">
                 <h2>Shop</h2>
             </div>
             <div className="nav__icons">
-                <IconButton onClick={() => setCartOpen(true)}>
-                    <Badge badgeContent={getTotalItems(cartItems)} color="error">
+                <IconButton onClick={() => navigate("/cart")}>
+                    <Badge badgeContent={totalItems} color="error">
                         <AddShoppingCartIcon />
                     </Badge>
                 </IconButton>
@@ -28,3 +36,5 @@ export default function Nav({ cartItems, setCartOpen, getTotalItems }) {
         </nav>
     );
 }
+
+export default Nav;
