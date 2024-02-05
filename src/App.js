124b524629920b80f4625e397fbe2f8e46e19dac
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-05 16:37:11 +0900

    feat: Implement detail page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes, Outlet } from "react-router-dom";
 import Nav from "./components/Nav";
 import MainPage from "./pages/MainPage";
+import CartPage from "./pages/CartPage";
+import DetailPage from "./pages/DetailPage";
 import "./App.css";
 
 const Layout = () => {
@@ -22,6 +24,8 @@ function App () {
                 <Routes>
                     <Route path="/" element={<Layout />}>
                         <Route index element={<MainPage />} />
+                        <Route path="/cart" element={<CartPage />} />
+                        <Route path="/detail/:id" element={<DetailPage />} />
                     </Route>
                 </Routes>
             </div>
