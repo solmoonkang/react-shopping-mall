344e1834a578f7a6c9322cf1500a12f4020bb88d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-05 16:48:12 +0900

    feat: Implement cart page ui if cart is empty or not

--- src/pages/CartPage/index.js
+++ src/pages/CartPage/index.js
@@ -3,6 +3,8 @@ import CartItem from "../../components/CartItem";
 import styled from "styled-components";
 import { useSelector, useDispatch } from "react-redux";
 import { addToCart, removeFromCart } from "../../actions/cartActions";
+import { useNavigate } from "react-router-dom";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 
 // 여기는 우측 상단에 있는 장바구니 버튼을 누르면 열리는 곳으로, 현재 등록된 상품과 해당 상품들의 총 가격을 출력한다.
 
@@ -10,6 +12,7 @@ const CartPage = () => {
     
     const cartItems = useSelector(state => state.cart);
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const handleRemoveFromCart = (id) => {
         dispatch(removeFromCart(id));
@@ -24,12 +27,21 @@ const CartPage = () => {
 
     return (
         <Wrapper>
-            <h2>장바구니</h2>
-            {cartItems.length === 0 ? <p>현재 등록된 상품이 없습니다.</p> : null}
-            {cartItems.map((item) => (
-                <CartItem key={item.id} item={item} addToCart={handleAddToCart} removeFromCart={handleRemoveFromCart} />
-            ))}
-            <h2>총 가격 : ${calculateTotal(cartItems).toFixed(2)}</h2>
+            {cartItems.length === 0 ? (
+                <EmptyCart>
+                    <ShoppingCartIcon style={{ fontSize: 100 }} />
+                    <h3>Cart가 비어있습니다.</h3>
+                    Cart에 상품을 넣어주세요.
+                    <p onClick={() => navigate("/")}>쇼핑 계속하기</p>
+                </EmptyCart>
+            ) : (
+                <>
+                    {cartItems.map((item) => (
+                        <CartItem key={item.id} item={item} addToCart={handleAddToCart} removeFromCart={handleRemoveFromCart} />
+                    ))}
+                    <h2>총 가격 : ${calculateTotal(cartItems).toFixed(2)}</h2>
+                </>
+            )}
         </Wrapper>
     );
 }
@@ -40,4 +52,26 @@ const Wrapper = styled.aside`
   font-family: Arial, Helvetica, sans-serif;
   width: 500px;
   padding: 20px;
+`;
+
+const EmptyCart = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+
+  img {
+    width: 100px;
+    height: 100px;
+  }
+
+  h3, p {
+    margin-top: 10px 0;
+  }
+
+  p {
+    color: blue;
+    cursor: pointer;
+  }
 `;
