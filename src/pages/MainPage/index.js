2d3c169433a84993ac9572a2ba96167b75217096
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-05 14:06:13 +0900

    feat: Implement main page

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Banner from "../../components/Banner";
+import Row from "../../components/Row";
 
 function MainPage() {
 
     return (
         <div>
-            
-            
+            <Banner />
+            <Row />
         </div>
     )
 }
