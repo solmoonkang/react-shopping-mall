9233ff0909b7ef1ed2e419f3b97c9ceca8d4e9e7
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-05 14:06:47 +0900

    feat: Implement store

--- src/store.js
+++ src/store.js
@@ -1,6 +1,15 @@
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 import cartReducer from "./reducers/cartReducer";
+import itemsReducer from "./reducers/itemsReducer";
+import categoryReducer from "./reducers/categoryReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(cartReducer);
+const rootReducer = combineReducers({
+    cart: cartReducer,
+    category: categoryReducer,
+    items: itemsReducer
+});
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
