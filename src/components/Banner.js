d4fb674a46aa521610da21302ffd2efac5b007ad
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-05 14:07:16 +0900

    feat: Implement banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,44 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { useDispatch } from "react-redux";
-import { loadCategory } from "../actions/loadCategory";
 import "./Banner.css";
-import axios from "axios";
-import requests from '../api/requests';
+import { selectCategory } from '../actions/categoryActions';
 
 // Banner 컴포넌트: 모두, 전자기기, 쥬얼리, 남성의류, 여성의류 등의 카테고리 버튼을 담당한다. 
 // 사용자가 버튼을 클릭하면 해당 카테고리의 상품들이 보여질 수 있도록 이벤트를 처리한다.
 
-const categories = ["모두", "전자기기", "쥬얼리", "남성의류", "여성의류"];
+const categories = {
+    "fetchAllProducts": "모두", 
+    "fetchElectronics": "전자기기", 
+    "fetchJewelery": "쥬얼리", 
+    "fetchMenClothing": "남성의류", 
+    "fetchWomenClothing": "여성의류"
+};
 
 function Banner() {
 
+    const [selectedKey, setSelectedKey] = useState(null);
     const dispatch = useDispatch();
 
-    const handleClick = async (category) => {
-        const products = await axios.get(requests.fetchAllProducts);
-        const items = products.filter(item => item.category === category);
-        dispatch(loadCategory(category, items));
-    };
+    const handleClick = useCallback((categoryName, categoryKey) => {
+        setSelectedKey(categoryKey);
+        dispatch(selectCategory(categoryKey));
+    }, [dispatch]);
+
+    useEffect(() => {
+        handleClick("모두", "fetchAllProducts");
+    }, [handleClick]);
 
     return (
-        <div>
-            {categories.map((category) => (
-                <button onClick={() => handleClick(category)}>{category}</button>
-            ))}
+        <div className="banner">
+            <h1>Products</h1>
+            <div className='banner-buttons'>
+                {Object.keys(categories).map(key  => (
+                    <button 
+                    className={`banner__button ${selectedKey === key ? "banner__button--selected" : ""}`}
+                    key={key} 
+                    onClick={() => handleClick(categories[key], key)}>
+                        {categories[key]}
+                    </button>
+                ))}
+            </div>
         </div>
     );
 }
 
 export default Banner;
-
-
-
-// const [cartItems, setCartItems] = useState([]);
-    // const { data, isLoading, error } = useQuery("products", getProducts);
-
-    // const getProducts = async () => {
-    //     return await (await fetch("https://fakestoreapi.com/products")).json();
-    // }
-
-    // if (isLoading) return <LinearProgress />;
-    // if (error) return <div>ERROR!</div>
