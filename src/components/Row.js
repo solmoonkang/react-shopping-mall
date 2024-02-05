3827a8a5be1095776c6797808c2c3009e366d79d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-05 15:00:29 +0900

    feat: Implement row component css styling

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { addToCart } from "../actions/cartActions";
 import axios from "../api/axios";
-import { Grid, Button } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import styled from "styled-components";
 import requests from "../api/requests";
 import { useDispatch, useSelector } from "react-redux";
@@ -9,21 +9,19 @@ import { useDispatch, useSelector } from "react-redux";
 // Row 컴포넌트: Banner 컴포넌트에서 클릭한 카테고리 버튼에 해당하는 상품들을 나열한다.
 // Row 컴포넌트는 선택된 카테고리에 따라 동적으로 상품 목록을 갱신하게 된다.
 
-// Commit 처리하고, Row 스타일링 구현해주기
-// 장바구니 아이콘 눌렀을 경우, CartPage로 이동하도록 구현해주기
-// 마찬가지로, 로그인 아이콘 눌렀을 경우, UserPage로 이동하도록 구현해주기
+// TODO: CSS Styling 이 제대로 적용되지 않았으므로 수정이 필요하다.
 
 const Product = ({ product, handleAddToCart }) => (
-    <Grid item key={product.id} xs={3}>
+    <Products item key={product.id} xs={3}>
         <ProductImage>
             <img src={product.image} alt={product.title} />
         </ProductImage>
-        <div>
-            <h3>{product.title}</h3>
-            <h3>{product.price}</h3>
-        </div>
-        <Button onClick={() => handleAddToCart(product)}>장바구니 추가</Button>
-    </Grid>
+        <ProductTitle>{product.title}</ProductTitle>
+        <ProductDetails>
+            <ProductButton onClick={() => handleAddToCart(product)}>장바구니에 담기</ProductButton>
+            <ProductPrice>$ {product.price}</ProductPrice>
+        </ProductDetails>
+    </Products>
 )
 
 const Row = () => {
@@ -52,6 +50,7 @@ const Row = () => {
     return (
         <Wrapper>
             <div className="grid-container">
+                <div>Showing: {products.length} items</div>
                 <Grid container spacing={5}>
                     {products.map((product) => (
                         <Product key={product.id} product={product} handleAddToCart={handleAddToCart} />
@@ -64,23 +63,62 @@ const Row = () => {
 
 export default Row;
 
-const Wrapper = styled.div`
+const Products = styled(Grid)`
+  border: 1px solid black;
+  border-radius: 4px;
+  overflow: hidden;
   display: flex;
-  justify-content: space-between;
   flex-direction: column;
+  justify-content: space-between;
   width: 100%;
-  border: 1px solid lightblue;
-  border-radius: 20px;
-  height: 100%;
+  height: 400px;
+  margin: 1rem;
+`;
 
-  button {
-    border-radius: 0 0 20px 20px;
-  }
+const ProductImage = styled.div`
+  width: 100%;
+  height: 200px;
+  overflow: hidden
+  margin: auto;
 
   img {
-    max-height: 200px;
+    width: 100%;
+    height: 100%;
     object-fit: cover;
-    border-radius: 20px 20px 0 0;
+  }
+`;
+
+const ProductTitle = styled.h3`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  text-align: center;
+`;
+
+const ProductDetails = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const ProductPrice = styled.h3`
+  margin-bottom: 1rem;
+`;
+
+const ProductButton = styled.button`
+  width: 60%;
+  height: 40px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  border-radius: 20px;
+
+  > div:first-child {
+    margin-bottom: 1rem;
   }
 
   div {
@@ -88,17 +126,5 @@ const Wrapper = styled.div`
     padding: 1rem;
     height: 100%;
     margin-top: 10px;
-  }
-`;
-
-const ProductImage = styled.div`
-  width: 100%;
-  height: 200px;
-  overflow: hidden
-
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
   }
 `;
