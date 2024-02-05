4d9ac99ecd0d842bc73f80c8137177e944e0ec03
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-05 21:48:06 +0900

    feat: Implement root reducer

--- src/reducers/rootProducer.js
+++ src/reducers/rootProducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import cartReducer from "./cartReducer";
 import itemsReducer from "./itemsReducer";
 import categoryReducer from "./categoryReducer";
+import userReducer from "./userReducer";
 
 export default combineReducers({
     cart: cartReducer,
     category: categoryReducer,
     items: itemsReducer,
+    user: userReducer
 });
