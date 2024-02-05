60eac8ea1f69d05387d9d4362ab8d63057f1bfaf
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-05 22:18:35 +0900

    feat: Implement if logout all products clear

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,8 +7,10 @@ import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import { Badge, IconButton } from "@material-ui/core";
 import { useSelector} from "react-redux";
 import "./Nav.css";
-// import LoginIcon from '@mui/icons-material/Login';
-// import LogoutIcon from '@mui/icons-material/Logout';
+import { logout } from "../actions/userActions";
+import { clearCart } from "../actions/cartActions";
+import { getAuth, signOut } from "firebase/auth";
+import { useDispatch } from "react-redux";
 
 // NAV 컴포넌트: 로고, 장바구니, 사용자 페이지, 로그인 버튼 등의 네비게이션 요소를 담당한다.
 
@@ -18,6 +20,21 @@ function Nav() {
     const totalItems = cartItems.reduce((count, item) => count + item.quantity, 0);
     const loggedIn = useSelector((state) => state.user.loggedIn);
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const handleLogout = async () => {
+        const auth = getAuth();
+        
+        try {
+            await signOut(auth);
+            dispatch(logout());
+            dispatch(clearCart());
+            alert('로그아웃이 성공적으로 완료되었습니다.');
+        } catch (error) {
+            console.log(error);
+            alert('로그아웃에 실패하였습니다.');
+        }
+    }
     
     return (
         <nav className="nav">
@@ -34,7 +51,7 @@ function Nav() {
                     <PersonIcon />
                 </IconButton>
                 <IconButton onClick={() => navigate("/auth/login")}>
-                    {loggedIn ? <ExitToAppIcon /> : <AccountCircleIcon />}
+                    {loggedIn ? <ExitToAppIcon onClick={handleLogout} /> : <AccountCircleIcon />}
                 </IconButton>
             </div>
         </nav>
