1140c937294c6e8df6c3824e0a2786c9c290025b
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-06 12:53:38 +0900

    feat: Remove Router component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route, Routes, Outlet } from "react-router-dom";
+import { Route, Routes, Outlet } from "react-router-dom";
 import Nav from "./components/Nav";
 import MainPage from "./pages/MainPage";
 import CartPage from "./pages/CartPage";
