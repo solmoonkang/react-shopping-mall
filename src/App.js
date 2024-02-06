1460329ff4156d08175363e66d14e18c7b6084a9
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-06 11:45:18 +0900

    fix: Remove Router component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Outlet } from "react-router-dom";
+import { Router, Route, Routes, Outlet } from "react-router-dom";
 import Nav from "./components/Nav";
 import MainPage from "./pages/MainPage";
 import CartPage from "./pages/CartPage";
 import DetailPage from "./pages/DetailPage";
 import UserPage from "./pages/UserPage";
 import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 import "./App.css";
 import "./.firebase/firebaseConfig.js";
 
@@ -23,20 +23,18 @@ const Layout = () => {
 function App () {
 
     return (
-        <Router>
-            <div className="app">
-                <Routes>
-                    <Route path="/" element={<Layout />}>
-                        <Route index element={<MainPage />} />
-                        <Route path="/cart" element={<CartPage />} />
-                        <Route path="/user" element={<UserPage />} />
-                        <Route path="/auth/login" element={<LoginPage />} />
-                        <Route path="/auth/signup" element={<SignupPage />} />
-                        <Route path="/detail/:id" element={<DetailPage />} />
-                    </Route>
-                </Routes>
-            </div>
-        </Router>
+        <div className="app">
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<MainPage />} />
+                    <Route path="/cart" element={<CartPage />} />
+                    <Route path="/user" element={<UserPage />} />
+                    <Route path="/auth/login" element={<LoginPage />} />
+                    <Route path="/auth/signup" element={<SignupPage />} />
+                    <Route path="/detail/:id" element={<DetailPage />} />
+                </Route>
+            </Routes>
+        </div>
     );
 }
 
